a8c23adf1111d8f9cc696f8c1e5c38b8c7a091cc
PhanTask <jinmeng.rao@wisc.edu>
2020-03-28 22:36:38 -0500

    Activity 9
    Activity 9

--- js/main.js
+++ js/main.js
@@ -1,155 +1,55 @@
-//execute script when window is loaded
-window.onload = function(){
-    //SVG dimension variables
-    var w = 900, h = 500;
-    //Example 1.2 line 1...container block
-    var container = d3.select("body") //get the <body> element from the DOM
-      .append("svg") //put a new svg in the body
-      .attr("width", w) //assign the width
-      .attr("height", h) //assign the height
-      .attr("class", "container") //always assign a class (as the block name) for styling and future selection
-      .style("background-color", "rgba(0,0,0,0.2)"); //only put a semicolon at the end of the block!
-      //innerRect block
-    var innerRect = container.append("rect") //put a new rect in the svg
-        .datum(400) //a single value is a datum
-        .attr("width", function(d){ //rectangle width
-            return d * 2; //400 * 2 = 800
-        })
-        .attr("height", function(d){ //rectangle height
-            return d; //400
-        })
-        .attr("class", "innerRect") //class name
-        .attr("x", 50) //position from left on the x (horizontal) axis
-        .attr("y", 50) //position from top on the y (vertical) axis
-        .style("fill", "#FFFFFF"); //fill color
-    var cityPop = [
-        {
-            city: 'Madison',
-            population: 233209
-        },
-        {
-            city: 'Milwaukee',
-            population: 594833
-        },
-        {
-            city: 'Green Bay',
-            population: 104057
-        },
-        {
-            city: 'Superior',
-            population: 27244
-        }
-    ];
+//begin script when window loads
+window.onload = setMap();
 
-    //above Example 2.8 line 20
-    //find the minimum value of the array
-    var minPop = d3.min(cityPop, function(d){
-        return d.population;
-    });
+//set up choropleth map
+function setMap(){
+    // map frame dimensions
+    var width = 1000,
+        height = 800;
 
-    //find the maximum value of the array
-    var maxPop = d3.max(cityPop, function(d){
-        return d.population;
-    });
+    //create new svg container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
 
-    //above Example 2.8 line 20
-    var x = d3.scaleLinear() //create the scale
-        .range([90, 750]) //output min and max
-        .domain([0, 3]); //input min and max
+    //create Albers equal area conic projection centered on China
+    var projection = d3.geoConicEqualArea()
+        .center([31.8084676, 104.8607508]) // 31.8084676, 104.8607508
+        .rotate([-50, 50, 0])
+        .parallels([20, 60])
+        .scale(1000)
+        .translate([-width / 4, height / 2]);
 
-    //scale for circles center y coordinate
-    var y = d3.scaleLinear()
-        .range([450, 50]) //was 440, 95
-        .domain([0, 700000]); //was minPop, maxPop
+    var path = d3.geoPath()
+        .projection(projection);
 
-    var color = d3.scaleLinear()
-        .range([
-            "#FDBE85",
-            "#D94701"
-        ])
-        .domain([
-            minPop,
-            maxPop
-        ]);
 
-    //below Example 3.5...create y axis generator
-    var yAxis = d3.axisLeft(y);
+    //use Promise.all to parallelize asynchronous data loading
+    var promises = [d3.csv("data/attr.csv"),
+                    d3.json("data/china.topojson")
+                   ];
+    Promise.all(promises).then(callback);
 
-    //create axis g element and add axis
-    var axis = container.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(50, 0)");
+    function callback(data){
+    csvData = data[0];
+    china = data[1];
+        console.log(csvData);
+        console.log(china);
+    //translate china TopoJSON
+    var chinaProvinces = topojson.feature(china, china.objects.china).features;
+    //examine the results
+    console.log(chinaProvinces);
 
-    yAxis(axis);
-
-    //Example 2.6 line 3
-    var circles = container.selectAll(".circles") //create an empty selection
-        .data(cityPop) //here we feed in an array
-        .enter() //one of the great mysteries of the universe
-        .append("circle") //inspect the HTML--holy crap, there's some circles there
-        .attr("class", "circles")
-        .attr("id", function(d){
-            return d.city;
-        })
-        .attr("r", function(d){
-            //calculate the radius based on population value as circle area
-            var area = d.population * 0.01;
-            return Math.sqrt(area/Math.PI);
-        })
-        .attr("cx", function(d, i){
-            //use the scale generator with the index to place each circle horizontally
-            return x(i);
-        })
-        .attr("cy", function(d){
-            return y(d.population);
-        })
-        .style("fill", function(d, i){ //add a fill based on the color scale generator
-            return color(d.population);
-        })
-        .style("stroke", "#000"); //black circle stroke
-
-    //below Example 3.9...create a text element and add the title
-    var title = container.append("text")
-        .attr("class", "title")
-        .attr("text-anchor", "middle")
-        .attr("x", 450)
-        .attr("y", 30)
-        .text("City Populations");
-
-    //Example 3.14 line 1...create circle labels
-    var labels = container.selectAll(".labels")
-        .data(cityPop)
+    //add China provinces to map
+    var provinces = map.selectAll(".provinces")
+        .data(chinaProvinces)
         .enter()
-        .append("text")
-        .attr("class", "labels")
-        .attr("text-anchor", "left")
-        .attr("y", function(d){
-            //vertical position centered on each circle
-            return y(d.population);
-        });
-
-    //first line of label
-    var nameLine = labels.append("tspan")
-        .attr("class", "nameLine")
-        .attr("x", function(d,i){
-            //horizontal position to the right of each circle
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+        .append("path")
+        .attr("class", function(d){
+            return "provinces " + d.properties.id;
         })
-        .text(function(d){
-            return d.city;
-        });
-
-    //create format generator
-    var format = d3.format(",");
-
-    //Example 3.16 line 1...second line of label
-    var popLine = labels.append("tspan")
-        .attr("class", "popLine")
-        .attr("x", function(d,i){
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .attr("dy", "15") //vertical offset
-        .text(function(d){
-            return "Pop. " + format(d.population); //use format generator to format numbers
-        });
+        .attr("d", path);
+    };
 };
